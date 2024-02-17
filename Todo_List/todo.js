e14b16e37f3f86c75e3625fabd370ecb460413ab
raghuvaran11 <raghuvaranmanthena@gmail.com>
2024-02-17 22:07:51 +0530

    todo-list

---
+++ Todo_List/todo.js
@@ -0,0 +1,15 @@
+var addbtn=document.getElementById("add-task")
+var taskinput=document.getElementById("task")
+var tasklist=document.getElementById("tasks")
+
+addbtn.addEventListener("click",function(){
+    var task=taskinput.value 
+    addtask(task)
+    taskinput.value=""
+})
+
+function addtask(task){
+    var li=document.createElement("li")
+    li.innerHTML=task
+    tasklist.appendChild(li)
+}
